986e7eb1374c9667e21b5005277f24dc578d324f
Michael Beckwith <tw2113@gmail.com>
2017-12-15 13:39:07 -0600

    initial commit

---
+++ custom-post-type-ui-capabilities.php
@@ -0,0 +1,20 @@
+<?php
+/**
+ * Custom Post Type UI Capabilities
+ *
+ * @since 1.0.0
+ *
+ * @package custom-post-type-ui-capabilities
+ */
+
+/*
+ * Plugin Name: Custom Post Type UI Capabilities
+ * Plugin URI: https://github.com/tw2113/custom-post-type-ui-capabilities
+ * Description: Admin panel for creating custom post types and custom taxonomies in WordPress
+ * Author: WebDevStudios
+ * Version: 1.5.6
+ * Author URI: https://webdevstudios.com/
+ * Text Domain: custom-post-type-ui
+ * Domain Path: /languages
+ * License: GPL-2.0+
+ */
---
+++ readme.txt
@@ -0,0 +1,15 @@
+=== Custom Post Type UI ===
+Contributors: tw2113
+Donate link: https://www.paypal.com/cgi-bin/webscr?cmd=_s-xclick&hosted_button_id=3084056
+Tags: custom post types, CPT, CMS, post, types, post type, taxonomy, tax, custom, content types, post types
+Requires at least: 4.9.1
+Tested up to: 4.9.1
+Stable tag: 1.0.0
+License: GPL-2.0+
+Requires PHP: 5.3
+
+Extends Custom Post Type UI to add capabilities sections.
+
+== Description ==
+
+Extends Custom Post Type UI to add capabilities sections.
